c1e314f991d3652069c57306f57dd31e5d11a73e
Zayka1971 <133379581+Zayka1971@users.noreply.github.com>
2023-05-26 16:53:48 +0300

    Add files via upload

---
+++ age_from_question.py
@@ -0,0 +1,16 @@
+from handlpiper.question.base_question import BaseQuestion
+
+
+class AgeFromQuestion(BaseQuestion):
+    def question(self):
+        return 'Укажите минимальный возраст для знакомства: '
+
+    def is_valid_answer(self, answer, params):
+        try:
+            answer = int(answer)
+            return 13 <= answer <= 100
+        except ValueError:
+            return False
+
+    def get_param_name(self):
+        return 'age_from'
